f8cad762cd541e8c723ecf5773cdbc51fe40fe6d
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-04-19 23:54:45 +0200

    bug fixes

--- base/webpack.dev.js
+++ base/webpack.dev.js
@@ -1,10 +1,18 @@
 const webpack = require("webpack");
+const path = require("path");
 const webpackMerge = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
 
-const ENV = process.env.NODE_ENV = process.env.ENV = "production";
+const ENV = process.env.NODE_ENV = process.env.ENV = "development";
 
 module.exports = webpackMerge(commonConfig, {
+
+    devtool: "cheap-module-eval-source-map",
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[hash].js"
+    },
 
     devServer: {
         contentBase: path.resolve(__dirname, "dist")

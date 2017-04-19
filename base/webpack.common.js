4650d8afd7c74bc712a72f7d193abd920b3d4b7a
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-04-20 00:34:07 +0200

    minor changes

--- base/webpack.common.js
+++ base/webpack.common.js
@@ -47,7 +47,11 @@ module.exports = {
         }),
 
         new webpack.optimize.CommonsChunkPlugin({
-            name: ["app", "vendor"]
+            name: "vendor",
+            minChunks: function (module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf("node_modules") !== -1;
+            }
         })
     ],
 

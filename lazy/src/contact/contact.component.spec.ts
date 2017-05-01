ce90b25886c64598188185751cce1d12f8f2befb
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-05-02 00:01:13 +0200

    minor changes

--- lazy/src/contact/contact.component.spec.ts
+++ lazy/src/contact/contact.component.spec.ts
@@ -1,38 +1,38 @@
 describe("Example", () => {
 
-  beforeEach(() => {
+	beforeEach(() => {
 
-  });
+	});
 
-  it("#setupDb should resolve a promise after the setup", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
+	it("#setupDb should resolve a promise after the setup", () => {
+		expect("a").toBe("a", "setupDb reject");
+	});
 
-  it("#insertUpdateSection", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
+	it("#insertUpdateSection", () => {
+		expect("a").toBe("a", "setupDb reject");
+	});
 
-  it("#deleteSection", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
+	it("#deleteSection", () => {
+		expect("a").toBe("a", "setupDb reject");
+	});
 
-  it("#getSections", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
+	it("#getSections", () => {
+		expect("a").toBe("a", "setupDb reject");
+	});
 
-  it("#insertUpdateTaskForSection", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
+	it("#insertUpdateTaskForSection", () => {
+		expect("a").toBe("a", "setupDb reject");
+	});
 
-  it("#deleteTaskForSection", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
+	it("#deleteTaskForSection", () => {
+		expect("a").toBe("a", "setupDb reject");
+	});
 
-  it("#getTasksForSections", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
+	it("#getTasksForSections", () => {
+		expect("a").toBe("a", "setupDb reject");
+	});
 
-  it("#mergeSectionsTasksDb", () => {
-    expect("a").toBe("a", "setupDb reject");
-  });
+	it("#mergeSectionsTasksDb", () => {
+		expect("a").toBe("a", "setupDb reject");
+	});
 });

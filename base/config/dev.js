1da82b18cb23a8461aa0f6c191bb1de1d5e8b1dc
Maurizio Cescon <Maurizio.CESCON@ext.ec.europa.eu>
2017-08-18 17:54:04 +0200

    minor changes

--- base/config/dev.js
+++ base/config/dev.js
@@ -27,6 +27,10 @@ module.exports = function (env) {
             clientLogLevel: "info",
             compress: true,
             contentBase: path.resolve(__dirname, "dist"),
+            inline: true,
+            historyApiFallback: true,
+            hot: true,
+            open: true,
             overlay: {
                 warnings: true,
                 errors: true

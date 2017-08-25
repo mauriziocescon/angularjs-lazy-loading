ad76b79819b636b438a9ab0d07b5d58cc96a010b
Maurizio Cescon <Maurizio.CESCON@ext.ec.europa.eu>
2017-08-25 14:59:36 +0200

    addiiton of roboto

--- base/src/vendor.ts
+++ base/src/vendor.ts
@@ -16,7 +16,6 @@ import "angular-translate-storage-local";
 import "angular-ui-router";
 import "babel-polyfill";
 import "oclazyload";
-import "roboto-fontface/css/roboto/sass/roboto-fontface-regular";
 import "stacktrace-js";
 
 // Take a look at http://thesassway.com/beginner/how-to-structure-a-sass-project

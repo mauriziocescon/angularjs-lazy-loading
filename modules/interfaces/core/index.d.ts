ef71f2d4fe550ab4b317e9e553f8cb20614ffb8b
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-11-12 01:35:43 +0100

    interfaces: preliminary implementation

---
+++ modules/interfaces/core/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./services/ui-utilities.service";
+export * from "./services/utilities.service";
---
+++ modules/interfaces/core/services/ui-utilities.service.d.ts
@@ -0,0 +1,37 @@
+/**
+ * UI Utilities
+ */
+export interface IUIUtilitiesService {
+    /**
+     * Get the currency symbol
+     * from the ISO code
+     *
+     * @param currency
+     */
+    getCurrencySymbol(currency: string): string;
+
+    /**
+     * Display a modal alert
+     *
+     * @param title title of modal
+     * @param message body of the modal
+     * @param buttonLabel label of the button
+     */
+    modalAlert(title: string, message: string, buttonLabel: string): void;
+    /**
+     * Display a confirm alert
+     *
+     * @param title title of modal
+     * @param message body of the modal
+     * @param yesButtonLabel label of the affermative button
+     * @param noButtonLabel label of the negative button
+     * @param callback function executed when the user click on a button with the result
+     */
+    modalConfirmer(title: string, message: string, yesButtonLabel: string, noButtonLabel: string, callback: (result: boolean) => void): void;
+    /**
+     * Display a toaster
+     *
+     * @param message body of the toster
+     */
+    toast(message: string): void;
+}

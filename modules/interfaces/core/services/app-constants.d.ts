3f8e75f1c4f5a0746e8ef7bd659119cee9bdf8b9
Maurizio Cescon <maurizio.cescon@gmail.com>
2017-11-16 20:31:05 +0100

    minor changes

---
+++ modules/interfaces/core/services/app-constants.d.ts
@@ -0,0 +1,27 @@
+/**
+ * Get application constants
+ * saved in app-constants.model
+ * grouped by field
+ */
+export interface IAppConstantsService {
+    /**
+     * Api
+     */
+    Api: Constants.Api;
+    /**
+     * Application
+     */
+    Application: Constants.Application;
+    /**
+     * Supported languages
+     */
+    Languages: Constants.Languages;
+    /**
+     * Local storage keys
+     */
+    LocalStorageKey: Constants.LocalStorageKey;
+    /**
+     * RegExp
+     */
+    Validator: Constants.Validator;
+}

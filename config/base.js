d7b148a90d197e70157c2911bbce3d73b452faa4
Maurizio Cescon <maurizio.cescon@gmail.com>
2023-11-12 14:00:41 +0100

    chore: source-map

--- config/base.js
+++ config/base.js
@@ -17,6 +17,8 @@ module.exports = (env) => {
       lazy: './src/lazy.ts',
     },
 
+    devtool: 'inline-source-map',
+
     resolve: {
       // Add '.ts' and '.tsx' as a resolvable extension.
       extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.scss', '.html', '.json'],
